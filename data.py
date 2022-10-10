eb397e98fda513aaf0b08559b3df2a4a8065dcdd
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-10 18:00:16 +0100

    data

---
+++ data.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as p, numpy,sklearn
+a=[]
+b=[]
+c=0
+b=range(10)
+print ("\033c\033[42;30m wait...\n")
+
+for n in b:
+	a=a+[[2,n]]
+	c=n*2
+	b=b+[c]
+	
+print (a)
+print (b)
