1d36c28d85efabd38f20d3ed1530e8bed589585e
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-10 20:11:47 +0100

    Add files via upload

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as p, numpy,sklearn
 a=[]
 b=[]
 c=0
-b=range(10)
+bb=range(10)
 print ("\033c\033[42;30m wait...\n")
 
-for n in b:
+for n in bb:
 	a=a+[[2,n]]
 	c=n*2
 	b=b+[c]
