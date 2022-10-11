01844c40601c0299166ed17cbc9dfae13591f620
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-11 17:57:50 +0100

    simulate sensor input from header cloud

---
+++ sensor.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as p, numpy,sklearn
+a=[]
+b=[]
+c=0
+bb=range(10)
+print ("\033c\033[42;30m wait...\n")
+
+for n in bb:
+	a=a+[[1,n]]
+	c=n*1
+	b=b+[c]
+	a=a+[[2,n]]
+	c=n*2
+	b=b+[c]
+	a=a+[[3,n]]
+	c=n*3
+	b=b+[c]
+	a=a+[[4,n]]
+	c=n*4
+	b=b+[c]
+	
+for n in range(len(b)):
+	print (a[n])
+	print (b[n])
